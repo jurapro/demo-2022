77edd81b1f560b0df245c7f6418b17d9effff0b1
jurapro <gszdfems@wsr.ru>
2022-02-16 07:26:51 -0800

    initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=gszdfems_m1',
+    'username' => 'gszdfems',
+    'password' => 'ZGcxyy',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
